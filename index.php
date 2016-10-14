173eba90a2caa895c25f365bf89be029bbcac7a6
dillerm <dillerm@oregonstate.edu>
2016-10-13 19:56:07 -0700

    dotabase main page now has random loadingscreen as background

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
 include "resources/base.php";
+
+$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(SITE_PATH . "dota-vpk/panorama/images/loadingscreens"));
+$screens = array(); 
+foreach ($files as $file) {
+    if (!$file->isDir())
+        $screens[] = $file->getPathname();
+}
+$screen = $screens[array_rand($screens)];
+$screen = str_replace(SITE_PATH . "dota-vpk", VPK_PATH, $screen);
+$screen = str_replace("\\", "/", $screen);
+
 include HEADER;
 ?>
 
 <h1>Dotabase</h1>
 
+
 <style>
 body {
 	background-size: 100% 100%;
-	background-image: url("http://104.131.152.50/dota-vpk/resource/flash3/images/workshop/loadingscreen_preview.png");
+	background-image: url("<?php echo $screen; ?>");
 }
 </style>
 
