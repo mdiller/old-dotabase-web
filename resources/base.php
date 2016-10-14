b52e8e3c8682b0962de10354d6026406efb27be7
Malcolm Diller <dillerm@engr.oregonstate.edu>
2016-10-13 18:32:02 -0700

    added new functionality and reorganized much of the framework

---
+++ resources/base.php
@@ -0,0 +1,55 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+defined("RESOURCES")
+	or define("RESOURCES", __DIR__ . "/");
+
+defined("SITE_PATH")
+	or define("SITE_PATH", RESOURCES . "../");
+
+defined("CSS_PATH")
+	or define("CSS_PATH", SITE_PATH . "css/");
+
+defined("JS_PATH")
+	or define("JS_PATH", SITE_PATH . "js/");
+
+defined("VPK_PATH")
+	or define("VPK_PATH", SITE_PATH . "dota-vpk/");
+
+defined("SITE_URL")
+	or define("SITE_URL", "http://" . $_SERVER['SERVER_NAME']);
+
+
+defined("HEADER")
+	or define("HEADER", RESOURCES . "header.php");
+defined("FOOTER")
+	or define("FOOTER", RESOURCES . "footer.php");
+
+include RESOURCES . "helpers.php";
+include RESOURCES . "dotabasepath.php";
+
+?>
+
+<!-- CSS File includes -->
+
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom CSS -->
+<style>
+<?php include CSS_PATH . "base.css"; ?>
+</style>
+
+
+<!--Javascript file includes -->
+
+<!-- jQuery Version 1.11.1 -->
+<script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Clipboard Javascript -->
+<script src="https://cdn.jsdelivr.net/clipboard.js/1.5.12/clipboard.min.js"></script>
+<!-- Custom Javascript -->
+<script>
+</script>
--- responses/index.php
+++ responses/index.php
@@ -1,44 +1,85 @@
-<?php 
-include "../templates/dotabasepath.php";
+<?php
+include "../resources/base.php";
 $dotabase = new PDO("sqlite:" . $dotabasepath);
 
 $keyphrase = @$_GET['keyphrase']?: '';
+$hero = @$_GET['hero']?: '';
+$concept = @$_GET['concept']?: '';
+$sortby = @$_GET['sortby']?: "text";
+$sortdir = @$_GET['sortdir']?: "DESC";
 
-$result = $dotabase->query("
-SELECT r.text, r.mp3, r.name, h.icon AS heroicon
+$sortby_dict = array(
+	"text" => "LENGTH(r.text)",
+	"rand" => "RANDOM()",
+	"crit" => "[group]",
+);
+$sortby = $sortby_dict[$sortby];
+
+$responses = $dotabase->query("
+SELECT DISTINCT r.text, r.mp3, r.name, h.icon AS heroicon, g.responserule_name as [group]
 FROM responses as r 
+LEFT JOIN responsegroupings as g ON r.fullname=g.response_fullname
 JOIN heroes as h ON r.hero_id=h.id 
-WHERE text LIKE '%" . $keyphrase . "%' 
-ORDER BY LENGTH(r.text) DESC 
+WHERE text LIKE '%" . $keyphrase ."%' 
+AND text!='' 
+AND h.name LIKE '%" . $hero . "%'
+AND responserule_name LIKE '%_" . $concept . "%'
+ORDER BY " . $sortby . " " . $sortdir ." 
 LIMIT 100");
 
-include "../templates/header.php";
+$herolist = $dotabase->query("SELECT localized_name, name from heroes");
+$conceptlist = $dotabase->query("SELECT name FROM criteria WHERE matchkey='Concept' ORDER BY name");
+
+include HEADER;
 ?>
 
 <form id="search" action="index.php" method="get">
 	<fieldset>
+		<input name="keyphrase" 
+			type="text" 
+			class="form-control" 
+			placeholder="Search for a key phrase here"
+			<?php echo form_value("keyphrase", "text"); ?> >
+		<select name="hero" class="form-control">
+			<option value="">Any Heroes</option>
+			<?php
+			foreach($herolist as $hero)
+				echo "<option " . form_value("hero", "select", $hero['name']) . ">" . $hero['localized_name'] . "</option>";
+			?>
+		</select>
+		<select name="concept" class="form-control">
+			<option value="">Any Concept</option>
+			<?php
+			foreach($conceptlist as $concept)
+				echo "<option" . form_value("concept", "select", $concept['name']) . ">" . $concept['name'] . "</option>";
+			?>
+		</select>
 		<div class="input-group">
-			<input type="text" 
-				name="keyphrase" 
-				class="form-control" 
-				placeholder="Search for a key phrase here"
-				value=<?php echo $keyphrase ?>>
-			<div class="input-group-btn">
-				<button type="submit" class="btn btn-primary">
-					<span class="glyphicon glyphicon-search">
-				</button>
+			<span class="input-group-addon" id="basic-addon1">Sort By:</span>
+			<select name="sortby" class="form-control">
+				<option <?php echo form_value("sortby", "select", "text"); ?>>Text Length</option>
+				<option <?php echo form_value("sortby", "select", "crit"); ?>>Criteria</option>
+				<option <?php echo form_value("sortby", "select", "rand"); ?>>Random</option>
+			</select>
+			<span class="input-group-addon">
+				<input id="sortdir" name="sortdir" type="checkbox" value="ASC" <?php echo form_value("sortdir", "checkbox"); ?>>
+				<label for="sortdir"></label>
 			</span>
 		</div>
+		<button type="submit" class="btn btn-primary">
+			<span class="glyphicon glyphicon-search"></span>
+			Search
+		</button>
 	</fieldset>
 </form>
 
 <?php 
-foreach($result as $response)
+foreach($responses as $response)
 {
 	include "response.php";
 }
 
-include "../templates/footer.php";
+include FOOTER;
 ?>
 <script>
 	// For discord copy buttons
