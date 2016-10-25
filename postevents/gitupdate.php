44ada6122dfa0fff62db44ae62dc05a538a6585f
dillerm <dillerm@oregonstate.edu>
2016-10-24 20:24:33 -0700

    added actual paths to gitupdate

--- postevents/gitupdate.php
+++ postevents/gitupdate.php
@@ -1,23 +1,22 @@
-
 <?php
+include "../resources/base.php";
 
 if (!empty($_REQUEST['payload'])) {
 	$data = json_decode($_REQUEST['payload'], true);
 	$repo = $data['repository']['full_name'];
 	switch($repo) {
 		case "mdiller/dotabase-web":
-			$path = "...";
+			$path = SITE_PATH;
 			break;
 		case "mdiller/dotabase":
-			$path = "...";
+			$path = DOTABASE_PATH;
 			break;
 		default:
 			echo "invalid repository: " . $repo;
 			die(1);
 	}
-	//$output = shell_exec("cd " . $path . "; git pull;");
-	$output = shell_exec("id -u -n");
-
+	$output = shell_exec("cd " . $path . "; git pull;");
+	
 	if(is_null($output)) {
 		echo "error executing pull";
 		die(1);
