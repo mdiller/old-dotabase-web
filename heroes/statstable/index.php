f7c58e9bddfd79681844b0f347df00e6ff57a1d0
dillerm <dillerm@oregonstate.edu>
2016-10-25 18:25:33 -0700

    fixed a few things on the hero stats table

--- heroes/statstable/index.php
+++ heroes/statstable/index.php
@@ -16,26 +16,24 @@ include HEADER;
 
 <table id="hero-table" class="table table-responsive table-header-rotated sortable">
 	<thead>
-		<tr>
-			<th data-defaultsort="asc"><div>Hero</div></th>
-			<th class="rotate-45"><div><span>Primary Attribute</span></div></th>
-			<th class="rotate-45"><div><span>Base Strength</span></div></th>
-			<th class="rotate-45"><div><span>Strength Gain</span></div></th>
-			<th class="rotate-45"><div><span>Base Intelligence</span></div></th>
-			<th class="rotate-45"><div><span>Intelligence Gain</span></div></th>
-			<th class="rotate-45"><div><span>Base Agility</span></div></th>
-			<th class="rotate-45"><div><span>Agility Gain</span></div></th>
-			<th class="rotate-45"><div><span>Base Health Regen</span></div></th>
-			<th class="rotate-45"><div><span>Base Armor</span></div></th>
-			<th class="rotate-45"><div><span>Base Movement</span></div></th>
-			<th class="rotate-45"><div><span>Turn Rate</span></div></th>
-			<th class="rotate-45"><div><span>Attack Range</span></div></th>
-			<th class="rotate-45"><div><span>Projectile Speed</span></div></th>
-			<th class="rotate-45"><div><span>Attack Damage Min</span></div></th>
-			<th class="rotate-45"><div><span>Attack Damage Max</span></div></th>
-			<th class="rotate-45"><div><span>Attack Rate</span></div></th>
-			<th class="rotate-45"><div><span>Attack Point</span></div></th>
-		</tr>
+		<th data-defaultsort="asc"><div>Hero</div></th>
+		<th class="rotate-45"><div><span>Primary Attribute</span></div></th>
+		<th class="rotate-45"><div><span>Base Strength</span></div></th>
+		<th class="rotate-45"><div><span>Strength Gain</span></div></th>
+		<th class="rotate-45"><div><span>Base Intelligence</span></div></th>
+		<th class="rotate-45"><div><span>Intelligence Gain</span></div></th>
+		<th class="rotate-45"><div><span>Base Agility</span></div></th>
+		<th class="rotate-45"><div><span>Agility Gain</span></div></th>
+		<th class="rotate-45"><div><span>Base Health Regen</span></div></th>
+		<th class="rotate-45"><div><span>Base Armor</span></div></th>
+		<th class="rotate-45"><div><span>Base Movement</span></div></th>
+		<th class="rotate-45"><div><span>Turn Rate</span></div></th>
+		<th class="rotate-45"><div><span>Attack Range</span></div></th>
+		<th class="rotate-45"><div><span>Projectile Speed</span></div></th>
+		<th class="rotate-45"><div><span>Attack Damage Min</span></div></th>
+		<th class="rotate-45"><div><span>Attack Damage Max</span></div></th>
+		<th class="rotate-45"><div><span>Attack Rate</span></div></th>
+		<th class="rotate-45"><div><span>Attack Point</span></div></th>
 	</thead>
 	<tbody>
 		<?php 
